a941395baaa21caca1aaba15f9a41ca9948b14db
Raynara Fonteles <98112838+RaynaraFonteles@users.noreply.github.com>
2023-11-02 16:13:59 -0300

    subindo o projeto pro github

---
+++ aula03/BlueJ/src/bluej/Arbusto.java
@@ -0,0 +1,17 @@
+package bluej;
+
+public class Arbusto{
+	
+    private String corPrimaria = "Verde Claro";
+    private String corSecundaria = "Verde Escuro";
+    private int coordX;
+    private int coordY;
+    private String tamanho;
+    
+    public Arbusto(int x, int y, String t){
+    	
+        coordX = x;
+        coordY = y;
+        tamanho = t;
+    }
+}
---
+++ aula03/BlueJ/src/bluej/animal.java
@@ -0,0 +1,17 @@
+package bluej;
+
+public class animal {
+	
+    private String especie;
+    private String raca;
+    private String nome;
+    private boolean Domestico;
+
+    public animal (String especie, String raca, String nome, boolean Domestico){
+    	
+        this.especie = especie;
+        this.raca = raca;
+        this.nome = nome;
+        this.Domestico = Domestico;
+    }
+}
